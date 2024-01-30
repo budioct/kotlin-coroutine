de9b189cda49f5aab7b641f52810186ef28034d7
Budhi Octaviansyah <budioct@gmail.com>
2024-01-30 11:20:27 +0700

    joinAll function

--- app/src/test/kotlin/com/tutorial/coroutine/JobTest.kt
+++ app/src/test/kotlin/com/tutorial/coroutine/JobTest.kt
@@ -3,6 +3,7 @@ package com.tutorial.coroutine
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 class JobTest {
 
@@ -65,6 +66,40 @@ class JobTest {
             job.cancel() // cancel() membatalkan job
             delay(3000) // menunggu proses job manual
         }
+    }
+
+    /**
+     * joinAll Function
+     * ● Kadang kita akan membuat coroutine lebih dari satu sekaligus
+     * ● Untuk menunggu semua Job coroutine selesai berjalan kita bisa menggunakan join() function
+     * ● Namun jika kita panggil satu-satu tiap Job coroutine nya, akan sangat mengganggu sekali
+     * ● Kotlin menyediakan joinAll(jobs) function untuk menunggu semua job selesai
+     */
+
+    @Test
+    fun testJoinAll() {
+
+        val time = measureTimeMillis {
+            runBlocking {
+                val job1 = GlobalScope.launch {
+                    delay(1000)
+                    println("Job 1: ${Thread.currentThread().name}")
+                }
+                val job2 = GlobalScope.launch {
+                    delay(1000)
+                    println("Job 2: ${Thread.currentThread().name}")
+                }
+
+                // cara ini bertele tele
+                // job1.join()
+                // job2.join()
+
+                // cara ringkas
+                joinAll(job1, job2) // joinAll(vararg jobs: Job): Unit = jobs.forEach { it.join() } // jalan secara aysnchronouse. bukan sequential
+            }
+        }
+        println("time: $time")
+
     }
 
 
